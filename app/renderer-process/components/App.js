ad93040a30004490cefb8eec97ab364844dd7b90
Yuya Ochiai <yuya0321@gmail.com>
2016-12-24 00:56:00 +0900

    Rewrite as function

--- app/renderer-process/components/App.js
+++ app/renderer-process/components/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import KanColle from './KanColle';
 
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<KanColle id={this.props.route.webviewId} src="http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"/>
-				<div>
-					{this.props.children}
-				</div>
-			</div>
-		);
-	}
-}
+const App = props => (
+	<div>
+		<KanColle id={props.route.webviewId} src="http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/"/>
+		<div>
+			{props.children}
+		</div>
+	</div>
+);
 
 App.propTypes = {
 	children: React.PropTypes.object,
--- app/renderer-process/components/Main.js
+++ app/renderer-process/components/Main.js
@@ -2,20 +2,16 @@ import React from 'react';
 import ScreenshotButton from './ScreenshotButton';
 import SettingsButton from './SettingsButton';
 
-class Main extends React.Component {
-	render() {
-		return (
-			<div>
-				<ScreenshotButton
-					targetId={this.props.route.webviewId}
-					screenshotDir={'.'}
-					/>
-				{' '}
-				<SettingsButton/>
-			</div>
-		);
-	}
-}
+const Main = props => (
+	<div>
+		<ScreenshotButton
+			targetId={props.route.webviewId}
+			screenshotDir={'.'}
+			/>
+		{' '}
+		<SettingsButton/>
+	</div>
+);
 
 Main.propTypes = {
 	route: React.PropTypes.object
