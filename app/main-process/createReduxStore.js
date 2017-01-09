96d0fd223e6aff69b84f130b5c1efe32da898077
Yuya Ochiai <yuya0321@gmail.com>
2017-01-10 00:11:14 +0900

    Set default screenshot directory

--- app/main-process/createReduxStore.js
+++ app/main-process/createReduxStore.js
@@ -1,8 +1,10 @@
+import {app} from 'electron';
 import fs from 'fs';
+import path from 'path';
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {electronEnhancer} from 'redux-electron-store';
 
-import {setWebviewScale} from '../actions';
+import {setWebviewScale, saveConfig} from '../actions';
 import reducers from '../reducers';
 import electronMiddleware from './middleware/electronMiddleware';
 import configManager, {configFile} from './middleware/configManager';
@@ -11,12 +13,14 @@ function createReduxStore(mainWindow) {
 	let store;
 
 	const config = (() => {
-		try {
-			return JSON.parse(fs.readFileSync(configFile, 'utf-8'));
-		} catch (e) {
-			console.error(e);
-			return {};
+		if (fs.existsSync(configFile)) {
+			try {
+				return JSON.parse(fs.readFileSync(configFile, 'utf-8'));
+			} catch (e) {
+				console.error(e);
+			}
 		}
+		return {screenshotDir: path.resolve(app.getPath('pictures'), 'FleetScreenshots')};
 	})();
 
 	const enhancer = compose(
@@ -30,6 +34,7 @@ function createReduxStore(mainWindow) {
 
 	store = createStore(combineReducers(reducers), initialState, enhancer);
 	store.dispatch(setWebviewScale(1.0));
+	store.dispatch(saveConfig('config'));
 
 	return store;
 }
