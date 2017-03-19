59d5dea2d7428863bc5fb237d4fb7c4b2ddf5c08
Yuya Ochiai <yuya0321@gmail.com>
2017-03-19 22:55:05 +0900

    Add reducers tests

---
+++ test/reducers/gameData_spec.js
@@ -0,0 +1,69 @@
+/* eslint-disable camelcase */
+
+import assert from 'assert';
+import * as actions from '../../app/actions';
+import gameData from '../../app/reducers/gameData';
+
+describe('gameData.js', function () {
+	describe('SET_KCSAPI_DECK_SHIP', function () {
+		beforeEach(function () {
+			this.data = {
+				user: {
+					api_deck_port: [{
+						api_ship: [11, 12, 13, 14, 15, 16]
+					}, {
+						api_ship: [21, 22, 23, 24, 25, 26]
+					}]
+				}
+			};
+		});
+
+		it('should handle selected ship', function () {
+			const newData1 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: 100,
+				api_id: 2,
+				api_ship_idx: 3
+			}));
+			assert.deepEqual(newData1.user.api_deck_port[1].api_ship, [21, 22, 23, 100, 25, 26]);
+
+			const newData2 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: 11,
+				api_id: 1,
+				api_ship_idx: 3
+			}));
+			assert.deepEqual(newData2.user.api_deck_port[0].api_ship, [14, 12, 13, 11, 15, 16]);
+		});
+
+		it('should handle data.api_ship_id === -1', function () {
+			// 編成画面で「はずす」を選択
+			const newData1 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: -1,
+				api_id: 1,
+				api_ship_idx: 1
+			}));
+			assert.deepEqual(newData1.user.api_deck_port[0].api_ship, [11, 13, 14, 15, 16, -1]);
+
+			const newData2 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: -1,
+				api_id: 2,
+				api_ship_idx: 3
+			}));
+			assert.deepEqual(newData2.user.api_deck_port[1].api_ship, [21, 22, 23, 25, 26, -1]);
+		});
+
+		it('should handle data.api_ship_id === -2', function () {
+			// 編成画面で「随伴艦一括解除」を選択
+			const newData1 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: -2,
+				api_id: 1
+			}));
+			assert.deepEqual(newData1.user.api_deck_port[0].api_ship, [11, -1, -1, -1, -1, -1]);
+
+			const newData2 = gameData(this.data, actions.setKcsapiDeckShip({
+				api_ship_id: -2,
+				api_id: 2
+			}));
+			assert.deepEqual(newData2.user.api_deck_port[1].api_ship, [21, -1, -1, -1, -1, -1]);
+		});
+	});
+});
