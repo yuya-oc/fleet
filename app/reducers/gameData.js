79fcad4a81e1a49d7febfe6cdbcaf816fcee7531
Yuya Ochiai <yuya0321@gmail.com>
2017-03-22 14:22:02 +0900

    Add some cases when set ship to deck

--- app/reducers/gameData.js
+++ app/reducers/gameData.js
@@ -3,10 +3,19 @@
 import {SET_KCSAPI_MASTER_DATA, SET_KCSAPI_USER_DATA, SET_KCSAPI_DECK, SET_KCSAPI_DECK_SHIP, SET_KCSAPI_PRESET_DECK, SET_KCSAPI_PRESET_SELECT, SET_KCSAPI_PRESET_REGISTER} from '../actions';
 
 export const initialState = {
-	master: {},
 	user: {},
 	presetDeck: {}
 };
+
+function searchShipFromDecks(apiDeckPort, shipId) {
+	for (let deckIndex = 0; deckIndex < apiDeckPort.length; deckIndex++) {
+		const orderIndex = apiDeckPort[deckIndex].api_ship.indexOf(shipId);
+		if (orderIndex > -1) {
+			return {deckIndex, orderIndex};
+		}
+	}
+	return null;
+}
 
 const gameData = (state = initialState, action) => {
 	switch (action.type) {
@@ -20,17 +29,30 @@ const gameData = (state = initialState, action) => {
 			});
 		case SET_KCSAPI_DECK_SHIP: {
 			const newState = Object.assign({}, state);
-			const fleetIndex = action.data.api_id - 1;
-			const shipIndex = action.data.api_ship_idx;
+			const deckIndex = action.data.api_id - 1;
+			const orderIndex = action.data.api_ship_idx;
 			switch (action.data.api_ship_id) {
 				case -1: // はずす
-					newState.user.api_deck_port[fleetIndex].api_ship.splice(shipIndex, 1, -1);
+					newState.user.api_deck_port[deckIndex].api_ship.splice(orderIndex, 1);
+					newState.user.api_deck_port[deckIndex].api_ship.push(-1);
 					break;
 				case -2: // 随伴艦一括解除
-					newState.user.api_deck_port[fleetIndex].api_ship.splice(1, 5, -1, -1, -1, -1, -1);
+					newState.user.api_deck_port[deckIndex].api_ship.splice(1, 5, -1, -1, -1, -1, -1);
 					break;
 				default: {
-					newState.user.api_deck_port[fleetIndex].api_ship[shipIndex] = action.data.api_ship_id;
+					const shipId = action.data.api_ship_id;
+					const existingShipId = newState.user.api_deck_port[deckIndex].api_ship[orderIndex];
+					const from = searchShipFromDecks(newState.user.api_deck_port, shipId);
+					newState.user.api_deck_port[deckIndex].api_ship[orderIndex] = shipId;
+					if (from) {
+						newState.user.api_deck_port[from.deckIndex].api_ship[from.orderIndex] = existingShipId;
+					}
+					newState.user.api_deck_port = newState.user.api_deck_port.map(deck => {
+						deck.api_ship = deck.api_ship.filter(v => v !== -1);
+						deck.api_ship.push(-1, -1, -1, -1, -1, -1);
+						deck.api_ship.length = 6;
+						return deck;
+					});
 					break;
 				}
 			}
@@ -47,20 +69,20 @@ const gameData = (state = initialState, action) => {
 			});
 		case SET_KCSAPI_PRESET_SELECT: {
 			const newState = Object.assign({}, state);
-			const fleetIndex = action.data.api_deck_id - 1;
+			const deckIndex = action.data.api_deck_id - 1;
 			const presetIndex = action.data.api_preset_no;
-			Object.assign(newState.user.api_deck_port[fleetIndex], newState.presetDeck.api_deck[presetIndex]);
+			Object.assign(newState.user.api_deck_port[deckIndex], newState.presetDeck.api_deck[presetIndex]);
 			return newState;
 		}
 		case SET_KCSAPI_PRESET_REGISTER: {
 			const newState = Object.assign({}, state);
-			const fleetIndex = action.data.api_deck_id - 1;
+			const deckIndex = action.data.api_deck_id - 1;
 			const presetIndex = action.data.api_preset_no;
 			newState.presetDeck.api_deck[presetIndex] = {
 				api_name: action.data.api_name,
 				api_name_id: action.data.api_name_id,
 				api_preset_no: action.data.api_preset_no,
-				api_ship: newState.user.api_deck_port[fleetIndex].api_ship.concat()
+				api_ship: newState.user.api_deck_port[deckIndex].api_ship.concat()
 			};
 			return newState;
 		}
