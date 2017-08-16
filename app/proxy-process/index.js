9573f806265b5741e07f68e447deefa3ffb701dd
Yuya Ochiai <yuya0321@gmail.com>
2017-08-17 00:52:23 +0900

    Rewrite proxy-process with hoxy

--- app/proxy-process/index.js
+++ app/proxy-process/index.js
@@ -1,28 +1,47 @@
-import url from 'url';
-import createProxyServer from '../main-process/createProxyServer';
-import kcsapi from '../lib/kcsapi';
+import hoxy from 'hoxy';
 
 const localProxyPort = process.argv[2];
 
-const proxyServer = createProxyServer().listen(localProxyPort, 'localhost', () => {
+const proxyServer = hoxy.createServer().listen(localProxyPort, 'localhost', () => {
 	process.send({event: 'listening'});
 });
 
-proxyServer.on('proxyReq', (proxyReq, req) => {
-	if (kcsapi.isKcsapiURL(req.url)) {
-		kcsapi.getRequestData(req, requestData => {
-//			Winston.silly(req.url);
-			kcsapi.getProxyResponseData(proxyReq, responseData => {
-				const pathname = url.parse(req.url).pathname;
-				process.send({
-					event: 'kcsapi',
-					pathname,
-					requestData,
-					responseData
-				});
-			});
-		});
+function parseIntInObject(object) {
+	const newObj = {};
+	for (let key in object) {
+		if (Object.prototype.hasOwnProperty.call(object, key)) {
+			const value = object[key];
+			if (typeof object[key] === 'object') {
+				newObj[key] = parseIntInObject(value);
+			} else if (value.match(/^-?[0-9]+$/)) {
+				newObj[key] = parseInt(value, 10);
+			} else {
+				newObj[key] = value;
+			}
+		}
 	}
+	return newObj;
+}
+
+proxyServer.intercept({
+	phase: 'request',
+	url: '/kcsapi/*',
+	as: 'params'
+});
+
+proxyServer.intercept({
+	phase: 'response',
+	url: '/kcsapi/*',
+	as: 'string'
+}, (req, resp) => {
+	const requestData = parseIntInObject(req.params);
+	const responseData = JSON.parse(resp.string.slice('svdata='.length));
+	process.send({
+		event: 'kcsapi',
+		pathname: req.url,
+		requestData,
+		responseData
+	});
 });
 
 process.on('exit', () => {
